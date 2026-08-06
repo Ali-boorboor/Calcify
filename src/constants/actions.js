05a7840dd84f5d29f7a48fd2f99a123417b237e4
Ali-boorboor <ali.w01rk@gmail.com>
2026-08-06 23:02:50 +0330

    refactor: extract calculator constants into separate files

---
+++ src/constants/actions.js
@@ -0,0 +1,13 @@
+const actions = {
+  ADD_DIGIT: "add-digit",
+
+  CHOOSE_OPERATION: "choose-operation",
+
+  CLEAR: "clear",
+
+  DELETE: "delete",
+
+  EVALUATE: "evaluate",
+};
+
+export default actions;
