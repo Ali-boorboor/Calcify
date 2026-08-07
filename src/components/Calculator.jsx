057dda2578d45a291ee1e4ca9d5e0a9677b99b08
Ali-boorboor <ali.w01rk@gmail.com>
2026-08-07 15:28:30 +0330

    refactor: semantic tags added

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,10 +3,12 @@ import KeyPad from "@/components/KeyPad";
 
 const Calculator = () => {
   return (
-    <main className="max-w-xs w-full bg-mist-600/60 font-mono font-semibold text-mist-100 border-2 border-mist-600 shadow-xs shadow-mist-600 rounded-md p-2 space-y-2">
-      <Display />
+    <main className="min-h-svh grid place-items-center bg-mist-400">
+      <section className="max-w-xs w-full bg-mist-600/60 font-mono font-semibold text-mist-100 border-2 border-mist-600 shadow-xs shadow-mist-600 rounded-md p-2 space-y-2">
+        <Display />
 
-      <KeyPad />
+        <KeyPad />
+      </section>
     </main>
   );
 };
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,10 +1,12 @@
 const Display = () => {
   return (
     <output
-      className="block max-h-22 w-full bg-mist-800/60 border-2 border-mist-800 shadow-xs shadow-mist-800 rounded-md py-2 px-4 aspect-video text-right"
+      className="grid grid-rows-[auto_1fr] items-center text-right max-h-22 w-full bg-mist-800/60 border-2 border-mist-800 shadow-xs shadow-mist-800 rounded-md py-2 px-4 aspect-video"
       aria-live="polite"
     >
-      1 + 4 = 5
+      <p className="text-sm line-clamp-1 break-all">1 +</p>
+
+      <p className="text-lg line-clamp-1 break-all">4</p>
     </output>
   );
 };
